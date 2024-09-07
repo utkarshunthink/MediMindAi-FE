cb32e7bb0daa7bf1942e89d550ebdc031cd13417
Utkarsh Srivastava <utkarsh.srivastava@unthinkable.co>
2024-09-07 19:58:55 +0530

    Initial project setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'MediMindAi-FE' title`, () => {
+  it(`should have the 'MediMindAi' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MediMindAi-FE');
+    expect(app.title).toEqual('MediMindAi');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, MediMindAi-FE');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, MediMindAi'
+    );
   });
 });
