d516d7ceb8d047fb4df09b5e29c7f26e2017e6d7
Utkarsh Srivastava <utkarsh.srivastava@unthinkable.co>
2024-09-07 20:17:44 +0530

    Landing and dashboard page setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'landing',
+    pathMatch: 'full',
+  },
+  {
+    path: 'landing',
+    loadComponent: () =>
+      import('./pages/landing/landing.page').then((p) => p.LandingPage),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./pages/dashboard/dashboard.page').then((p) => p.DashboardPage),
+  },
+];
---
+++ src/app/pages/landing/landing.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  imports: [],
+  templateUrl: './landing.page.html',
+  styleUrl: './landing.page.scss',
+})
+export class LandingPage {}
