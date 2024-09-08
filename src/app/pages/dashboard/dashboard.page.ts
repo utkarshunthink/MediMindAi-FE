4715d89ebc50218f063b5a0adf5154de002ff5f4
Utkarsh Srivastava <utkarsh.srivastava@unthinkable.co>
2024-09-08 17:42:25 +0530

    Dashboard BMI section implementation

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatProgressBarModule],
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
 })
-export class DashboardPage {}
+export class DashboardPage {
+  getBmiProgressBarValue() {
+    const inputValue = 24.9;
+    const minScaleValue = 15;
+    const maxScaleValue = 40;
+    const minProgressValue = 0;
+    const maxProgressValue = 100;
+
+    if (inputValue < minScaleValue) {
+      return minScaleValue;
+    }
+
+    if (inputValue > maxScaleValue) {
+      return maxScaleValue;
+    }
+
+    const progressValue =
+      ((inputValue - minScaleValue) / (maxScaleValue - minScaleValue)) *
+      (maxProgressValue - minProgressValue);
+    return progressValue;
+  }
+}
