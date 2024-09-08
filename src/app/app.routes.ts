36cbefedb0f2baf6a8088ade3a6e386e28d2f887
Utkarsh Srivastava <utkarsh.srivastava@unthinkable.co>
2024-09-08 13:15:39 +0530

    Updated routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,18 +2,22 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'landing',
-    pathMatch: 'full',
-  },
-  {
     path: 'landing',
     loadComponent: () =>
       import('./pages/landing/landing.page').then((p) => p.LandingPage),
   },
   {
-    path: 'dashboard',
+    path: '',
     loadComponent: () =>
-      import('./pages/dashboard/dashboard.page').then((p) => p.DashboardPage),
+      import('./pages/home/home.page').then((p) => p.HomePage),
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/dashboard/dashboard.page').then(
+            (p) => p.DashboardPage
+          ),
+      },
+    ],
   },
 ];
