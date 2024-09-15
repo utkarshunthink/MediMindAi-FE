5adc7779b64aae1d804eb9e5a3e49549832df9d4
Rishabh Vishwakarma <rishabh.vishwakarma@unthinkable.co>
2024-09-15 16:24:03 +0530

    success handler fix

--- presctiptionAI-backend/src/middlewares/responseHandler.js
+++ presctiptionAI-backend/src/middlewares/responseHandler.js
@@ -2,7 +2,7 @@ const successResponse = (res, data, message = 'Success', statusCode = 200) => {
     res.status(statusCode).json({
         success: true,
         message,
-        data
+        data: data || []
     });
 };
 
